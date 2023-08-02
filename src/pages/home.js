b58e7724052d5ae552e979174ba762b82f60f90d
Ayate10671 <ayatemohamed1582001@gmail.com>
2023-08-02 15:10:32 +0300

    edit

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function home() {
+  return (
+    <div>
+      
+      <h1>Welcome to Dashboard</h1>
+      
+    </div>
+  );
+}
---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Form from "../componants/form";
+
+export default function home() {
+  return (
+    <div>
+      
+      <Form/>
+      
+    </div>
+  );
+}
